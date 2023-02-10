aa5af3ab5654918971d481b057b881095c70d43b
Bruno Vellutini <organelas@gmail.com>
2023-02-10 09:15:54 +0100

    Add license notice to matlab tutorial files

--- scripts/TutorialIlastikDetectorSpherelikeFitter.m
+++ scripts/TutorialIlastikDetectorSpherelikeFitter.m
@@ -1,5 +1,28 @@
 %% ImSAnE: Tutorial with Ilastik Detector and Spherelike Fitter
+%
+% This file is part of ImSAnE.
 % 
+% ImSAnE is free software: you can redistribute it and/or modify
+% it under the terms of the GNU General Public License as published by
+% the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% ImSAnE is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU General Public License for more details.
+% 
+% You should have received a copy of the GNU General Public License
+% along with ImSAnE.  If not, see <http://www.gnu.org/licenses/>.
+%
+% We make an explicit exception giving permission to link this code
+% with GPL-incompatible matlab facilities.
+%
+% Copyright 2015 Idse Heemskerk and Sebastian Streichan
+% Modified 2022 by Bruno C. Vellutini
+% 
+%%
+%
 % Sample data: Insect embryo
 %
 % In this tutorial we detect the embryos apical surface using Ilastik and
